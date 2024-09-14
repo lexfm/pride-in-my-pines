e261b6c37630fac91b494ad3c5f2ff7fca97e89e
Alex Fimbres <lexfimbres@gmail.com>
2024-09-13 23:55:36 -0700

    feat: pretty placeholder indeed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Facebook, Instagram } from 'lucide-react'
 
 export default function Home() {
   return (
@@ -19,73 +20,48 @@ export default function Home() {
           height={700}
           unoptimized
         />
-        <h2 className="text-4xl font-extrabold dark:text-white">Now let's do our PIMP queer page!</h2>
+        <h2 className="text-4xl font-extrabold dark:text-white">More details coming soon!</h2>
 
         <div className="flex gap-6 items-center justify-center flex-wrap">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://www.instagram.com/prideinmypines/"
-            target="_blank"
+            href="/"
+            target="_self"
             rel="noopener noreferrer"
           >
-            Instagram
+            Info
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://www.facebook.com/prideinmypines"
-            target="_blank"
+            href="/"
+            target="_self"
             rel="noopener noreferrer"
           >
-            Facebook
+            Purchase
           </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="/"
+          href="https://www.instagram.com/prideinmypines/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Some
+          <Instagram />
+          Instagram
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="/"
+          href="https://www.facebook.com/prideinmypines"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Footer
+          <Facebook />
+          Facebook
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Items
-        </a>
+
       </footer>
     </div>
   );
