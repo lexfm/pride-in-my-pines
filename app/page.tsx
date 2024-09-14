757cbcc31e8017196b94fb66a41c452301142429
Alex Fimbres <lexfimbres@gmail.com>
2024-09-13 23:07:37 -0700

    feat: heading gradient shadow improv

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header className="flex items-center justify-center min-h-48 max-h-48 md:max-h-64 lg:max-h-32 pimp-bg overflow-hidden">
-        <h1 className="text-4xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-300 to-blue-600 drop-shadow-lg text-center">
+      <header className="relative flex items-center justify-center min-h-48 max-h-48 md:max-h-64 lg:max-h-32 pimp-bg overflow-hidden">
+        <div className="absolute inset-0 bg-black bg-opacity-40"></div> {/* Slight overlay */}
+        <h1 className="relative text-4xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-300 via-blue-600 to-yellow-500 drop-shadow-2xl text-center">
           Pride in My Pines
         </h1>
       </header>
