2dac1562b43708133adb5f289eb36547b8ba67cd
Alex Fimbres <lexfimbres@gmail.com>
2024-09-16 23:32:02 -0700

    feat: set up document HTML

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+// pages/_document.tsx
+
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+       <meta name="PIMP home pahe" content="The queerest most inclusive camping adventure born in beautiful San Diego CA"/>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
