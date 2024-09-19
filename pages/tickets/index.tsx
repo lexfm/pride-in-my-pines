fca925b8bdbdb1a26ac57ea7e44c25470643c068
Alex Fimbres <lexfimbres@gmail.com>
2024-09-18 22:49:15 -0700

    fix: link dep removed from tickets

--- pages/tickets/index.tsx
+++ pages/tickets/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Link from "next/link";
 import PaymentButtons from "../../components/paymentButtons";
 
 declare global {
