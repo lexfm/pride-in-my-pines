45639ef037a3b1d49134e6960442a2c989711bc2
Alex Fimbres <lexfimbres@gmail.com>
2024-10-01 23:55:35 -0700

    feat: To Bring guide page

---
+++ pages/bring/index.tsx
@@ -0,0 +1,16 @@
+
+import Image from "next/image";
+
+export default function ToBring() {
+    return (
+        <main className="to-bring-section flex flex-col gap-4 row-start-2 items-center items-start">
+            <Image
+                aria-hidden
+                src="/tobring.png"
+                alt="To Bring"
+                width={700}
+                height={900}
+            />
+        </main>
+    );
+}
