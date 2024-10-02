49a607caa55fc466dd2f640d015774eff2aeb993
Alex Fimbres <lexfimbres@gmail.com>
2024-10-02 01:05:40 -0700

    fix: To-bring chart

--- pages/bring/index.tsx
+++ pages/bring/index.tsx
@@ -6,7 +6,7 @@ export default function ToBring() {
         <main className="to-bring-section flex flex-col gap-4 row-start-2 items-center items-start">
             <Image
                 aria-hidden
-                src="/tobring.png"
+                src="/toBring.png"
                 alt="To Bring"
                 width={700}
                 height={900}
