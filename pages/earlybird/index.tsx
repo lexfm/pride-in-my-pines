95abf87e8d9d860b236931f0029038143a9c3980
Alex Fimbres <lexfimbres@gmail.com>
2024-10-15 23:46:20 -0700

    feat: next event refinements

--- pages/earlybird/index.tsx
+++ pages/earlybird/index.tsx
@@ -7,6 +7,8 @@ export default function EarlyBird() {
         <main className="earlybird-section flex flex-col gap-4 row-start-2 items-center items-start">
 
             <h3 className="text-2xl font-extrabold dark:text-white text-center">"The Magic Continues: Early Bird Adventure – May 2025!"🌈👥 </h3>
+            <h3 className="text-2xl font-extrabold dark:text-white text-center">🕺🎉 Get Ready to Flashback to the 80's! 🎉💃🏻</h3>
+            
             <Image
                 aria-hidden
                 src="/earlyBirdMay25.png"
@@ -17,7 +19,7 @@ export default function EarlyBird() {
             <div className="text-gray-400 text-xl leading-relaxed space-y-4 mt-5">
 
                 <p>
-                    Don’t miss out on the next unforgettable adventure! Join us for another weekend of fun, freedom, and friendship under the stars. Book early to lock in your spot for the last weekend of May 2025. Early bird tickets are flying away fast—snag yours now and get ready for the next chapter of magic! 🌈✨
+                    Don’t miss out on the next unforgettable adventure! Join us for another weekend of fun, freedom, and friendship under the stars—this time, with an epic 80’s theme! 🌟 Book early to lock in your spot for the last weekend of May 2025. Early bird tickets are flying away fast—snag yours now and get ready for the next chapter of magic! 🌈✨
                 </p>
             </div>
 
